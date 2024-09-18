4a3d861a7cbe4d15a09308d4d36d9ac6d48062cc
Beanssprouts <106742533+Beanssprouts@users.noreply.github.com>
2024-09-18 01:09:08 -0400

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-# import dependencies
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-from datetime import datetime
-
-class User(AbstractUser):
-    spotify_token = models.CharField(max_length=255, blank=True, null=True)
-    refresh_token = models.CharField(max_length=255, blank=True, null=True)
-    token_expiry = models.DateTimeField(default=datetime.min)
