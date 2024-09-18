d70dd9ae40807cf4c38fb4b22d3ae2280ae82760
Beanssprouts <106742533+Beanssprouts@users.noreply.github.com>
2024-09-18 01:09:00 -0400

    Delete serializers.py

--- serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import User
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'spotify_token', 'refresh_token', 'token_expiry']
